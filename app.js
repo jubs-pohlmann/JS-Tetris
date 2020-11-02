da1a83557f2f1d45cf0be521f13e487713aac88a
unknown <juliapohlmann@hotmail.com>
2020-11-01 21:04:38 -0300

    Arrow functions and forEach()

--- app.js
+++ app.js
@@ -6,4 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10;
 
     console.log(squares);
+
+
+
+    /* HOW TO USE FUNCTIONS:
+    function functionName(){
+        //code goes here
+    }
+    OR
+    functionName = function () {
+        //code goes here
+    }
+    OR
+    functionName = () => {
+        //code goes here
+    } 
+    HOW TO USE .FOREACH
+    array.forEach(variable => {
+        //code dealing with each variable inside array
+    })
+    */
 });
